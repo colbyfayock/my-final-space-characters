8bfa61c32c1bdace7515a93a14427108056f3814
colbyfayock <fayock@gmail.com>
2020-09-27 08:22:08 -0400

    Part 0: bootstrapping a new app with a list

--- src/App.js
+++ src/App.js
@@ -1,24 +1,57 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+
+const finalSpaceCharacters = [
+  {
+    id: 'gary',
+    name: 'Gary Goodspeed',
+    thumb: '/images/gary.png'
+  },
+  {
+    id: 'cato',
+    name: 'Little Cato',
+    thumb: '/images/cato.png'
+  },
+  {
+    id: 'kvn',
+    name: 'KVN',
+    thumb: '/images/kvn.png'
+  },
+  {
+    id: 'mooncake',
+    name: 'Mooncake',
+    thumb: '/images/mooncake.png'
+  },
+  {
+    id: 'quinn',
+    name: 'Quinn Ergon',
+    thumb: '/images/quinn.png'
+  }
+]
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Final Space Characters</h1>
+        <ul className="characters">
+          {finalSpaceCharacters.map(({id, name, thumb}) => {
+            return (
+              <li key={id}>
+                <div className="characters-thumb">
+                  <img src={thumb} alt={`${name} Thumb`} />
+                </div>
+                <p>
+                  { name }
+                </p>
+              </li>
+            );
+          })}
+        </ul>
       </header>
+      <p>
+        Images from <a href="https://final-space.fandom.com/wiki/Final_Space_Wiki">Final Space Wiki</a>
+      </p>
     </div>
   );
 }
